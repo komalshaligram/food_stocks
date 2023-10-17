3c61692528f6627b71bd215247679228756d83cc
komalshaligram <komal.a@shaligraminfotech.com>
2023-10-17 11:08:33 +0530

    - change package name - update google service info list

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -7,7 +9,6 @@ import 'package:food_stock/ui/utils/app_utils.dart';
 import 'package:food_stock/ui/utils/themes/app_img_path.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../data/storage/shared_preferences_helper.dart';
 
 class SplashRoute {
@@ -29,6 +30,25 @@ class SplashScreen extends StatelessWidget {
 class SplashScreenWidget extends StatelessWidget {
   const SplashScreenWidget({Key? key}) : super(key: key);
 
+  getVersion(SharedPreferencesHelper preferencesHelper) async{
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    debugPrint("package info : ${packageInfo.toString()}");
+    String version = packageInfo.version;
+    preferencesHelper.setAppVersion(version: version);
+    debugPrint("splash pref = ${preferencesHelper.getUserLoggedIn()}");
+  }
+
+  getDeviceId() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if(Platform.isAndroid){
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      print('Running on ${androidInfo.id}');
+    }else{
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      print('Running on ${iosInfo.utsname.machine}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<SplashBloc, SplashState>(
@@ -36,12 +56,9 @@ class SplashScreenWidget extends StatelessWidget {
         if (state.isRedirected) {
           SharedPreferencesHelper preferencesHelper = SharedPreferencesHelper(
               prefs: await SharedPreferences.getInstance());
+          getVersion(preferencesHelper);
+          getDeviceId();
           print(preferencesHelper.getUserLoggedIn());
-          PackageInfo packageInfo = await PackageInfo.fromPlatform();
-          debugPrint("package info : ${packageInfo.toString()}");
-          String version = packageInfo.version;
-          preferencesHelper.setAppVersion(version: version);
-          debugPrint("splash pref = ${preferencesHelper.getUserLoggedIn()}");
           if (preferencesHelper.getUserLoggedIn()) {
             Navigator.pushReplacementNamed(
                 context, RouteDefine.bottomNavScreen.name);
@@ -50,7 +67,7 @@ class SplashScreenWidget extends StatelessWidget {
                 context, RouteDefine.connectScreen.name);
           }
         }
-      },
+      }     ,
       child: BlocBuilder<SplashBloc, SplashState>(
         builder: (context, state) {
           return Scaffold(
