701b206f23adfbd27b4fca87538db8ee967d8277
VirajGelani <viraj.g@shaligraminfotech.com>
2023-12-14 16:10:21 +0530

    menu screen pagination and pull to refresh added.

--- lib/bloc/menu/menu_state.dart
+++ lib/bloc/menu/menu_state.dart
@@ -5,10 +5,18 @@ class MenuState with _$MenuState {
   const factory MenuState({
     required List<Content> contentList,
     required bool isShimmering,
+    required int pageNum,
+    required bool isLoadMore,
+    required bool isBottomOfAppContents,
+    required RefreshController refreshController,
   }) = _MenuState;
 
   factory MenuState.initial() => MenuState(
-        contentList: [],
+    contentList: [],
         isShimmering: false,
+        pageNum: 0,
+        isLoadMore: false,
+        isBottomOfAppContents: false,
+        refreshController: RefreshController(),
       );
 }
