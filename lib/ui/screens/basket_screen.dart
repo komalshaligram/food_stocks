84dc330c4a02f28e62eb627675791ee049061dde
komal-sit <komal.a@shaligraminfotech.com>
2024-02-07 12:22:06 +0530

    - bug fixing

--- lib/ui/screens/basket_screen.dart
+++ lib/ui/screens/basket_screen.dart
@@ -306,6 +306,7 @@ class BasketScreenWidget extends StatelessWidget {
                               ),
                             )
                           : SizedBox(),
+
                       state.isShimmering && state.basketProductList.isEmpty
                           ? BasketScreenShimmerWidget()
                           : (state.basketProductList.length) != 0
