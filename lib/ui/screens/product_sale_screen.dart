2c560209f62db762eae85190cb499134a76f5b30
komal-sit <komal.a@shaligraminfotech.com>
2024-05-23 12:40:40 +0530

    created list view

--- lib/ui/screens/product_sale_screen.dart
+++ lib/ui/screens/product_sale_screen.dart
@@ -10,6 +10,7 @@ import 'package:food_stock/ui/utils/themes/app_img_path.dart';
 import 'package:food_stock/ui/widget/common_product_list_widget.dart';
 import 'package:food_stock/ui/widget/common_product_sale_item_widget.dart';
 import 'package:food_stock/ui/widget/common_sale_description_dialog.dart';
+import 'package:food_stock/ui/widget/common_sale_listview.dart';
 import 'package:food_stock/ui/widget/common_shimmer_widget.dart';
 import 'package:food_stock/ui/widget/product_sale_screen_shimmer_widget.dart';
 import 'package:food_stock/ui/widget/sized_box_widget.dart';
@@ -154,8 +155,7 @@ class ProductSaleScreenWidget extends StatelessWidget {
                                       shrinkWrap: true,
                                       physics: const NeverScrollableScrollPhysics(),
                                       padding: EdgeInsets.symmetric(horizontal: AppConstants.padding_5),
-                                      itemBuilder: (context, index) => commonSaleListView(
-
+                                      itemBuilder: (context, index) => CommonSaleListView(
                                           context: context,
                                           discountedPrice: double.parse(state.productSalesList[index].sale.salePrice),
                                           isFromSale: state.productSalesList[index].sale.isSale,
@@ -172,6 +172,7 @@ class ProductSaleScreenWidget extends StatelessWidget {
                                             showProductDetails(
                                               context: context,
                                               productId: state.productSalesList[index].id,
+                                              productStock: state.productStockList[index].stock,
                                             );
                                           }),
                                     ),
@@ -220,204 +221,6 @@ class ProductSaleScreenWidget extends StatelessWidget {
     );
   }
 
-  Widget commonSaleListView({final double? height,
-    required double discountedPrice,
-  required final String productImage,
-  required final String productName,
-
-  required final double price,
-  required final String productStock,
-  required final void Function() onButtonTap,
-  required final bool isGuestUser,
-  required final String numberOfUnits,
-  required final String lowStock,
-  required final bool? isPesach,
-  required final bool? isFromSale,
-  required final BuildContext context,
-  required final String? salesDesc}){
-    debugPrint('price:${price.toString()}');
-    return GestureDetector(
-      onTap: onButtonTap,
-      child: Container(
-        width:double.maxFinite,
-        //  height: 150,
-        decoration: BoxDecoration(
-          color: AppColors.whiteColor,
-          borderRadius: BorderRadius.all(Radius.circular(AppConstants.radius_10)),
-          boxShadow: [
-            BoxShadow(
-                color: AppColors.shadowColor.withOpacity(0.15),
-                blurRadius: AppConstants.blur_10),
-          ],
-        ),
-        clipBehavior: Clip.hardEdge,
-        margin: EdgeInsets.symmetric(
-            vertical: AppConstants.padding_10,
-            horizontal: AppConstants.padding_5),
-        padding: EdgeInsets.symmetric(
-            vertical: AppConstants.padding_5,
-            horizontal: AppConstants.padding_10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            !isGuestUser ? productImage.isNotEmpty?  CachedNetworkImage(
-              imageUrl: "${AppUrls.baseFileUrl}$productImage",
-              height: 70,
-              width: 70,
-              fit: BoxFit.contain,
-              placeholder: (context, url) {
-                return CommonShimmerWidget(
-                  child: Container(
-                    height: 70,
-                    width: 70,
-                    decoration: BoxDecoration(
-                      color: AppColors.whiteColor,
-                      borderRadius: BorderRadius.all(
-                          Radius.circular(AppConstants.radius_10)),
-                    ),
-                  ),
-                );
-              },
-              errorWidget: (context, error, stackTrace) {
-                debugPrint('sale list image error : $error');
-                return Container(
-                  child: Image.asset(AppImagePath.imageNotAvailable5,
-                      height: 70, width: 70, fit: BoxFit.cover),
-                );
-              },
-            ) :
-            Image.asset(AppImagePath.imageNotAvailable5 , height: 70,
-              width: 70, ) :  Image.asset(AppImagePath.imageNotAvailable5 , height: 70,
-              width: 70, ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(width: 20),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                 Row(
-                   mainAxisAlignment: MainAxisAlignment.start,
-                   children: [
-                     Container(
-                       width:getScreenWidth(context)/2.5,
-                       child: Column(
-                         mainAxisAlignment: MainAxisAlignment.start,
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           Text(
-                             productName,
-                             style: AppStyles.rkBoldTextStyle(
-                                 size: AppConstants.font_14,
-                                 color: AppColors.blackColor,
-                                 fontWeight: FontWeight.w600),
-                             maxLines: 4,
-                             overflow: TextOverflow.ellipsis,
-                           ),
-                           (productStock) != '0' && lowStock.isEmpty ||   isGuestUser ? 0.width :
-                           (productStock) == '0' && lowStock.isNotEmpty ?Text(
-                             AppLocalizations.of(context)!
-                                 .out_of_stock1,
-                             style: AppStyles.rkBoldTextStyle(
-                                 size: AppConstants.font_14,
-                                 color: AppColors.redColor,
-                                 fontWeight: FontWeight.w400),
-                           ) : Text(
-                             lowStock,
-                             style: AppStyles.rkBoldTextStyle(
-                                 size: AppConstants.font_14,
-                                 color: AppColors.orangeColor,
-                                 fontWeight: FontWeight.w400),
-                           ),
-                           isPesach! ? 3.height :0.height,
-                           isPesachLabelShow(isPesach!, context),
-                           isPesach! ? 3.height :0.height,
-                           !isGuestUser ? numberOfUnits != '0' ? Text(
-                             '${numberOfUnits.toString()}${' '}${AppLocalizations.of(context)!.unit_in_box}',
-                             style: AppStyles.rkBoldTextStyle(
-                                 size: AppConstants.font_12,
-                                 color: AppColors.blackColor,
-                                 fontWeight: FontWeight.w400),
-                           ) : 0.width : 0.width,
-                         ],
-                       ),
-                     ),
-                     !isGuestUser ? Column(
-                       crossAxisAlignment: CrossAxisAlignment.center,
-                       children: [
-                      Text('${AppLocalizations.of(context)?.currency}${price.toStringAsFixed(2)}',  style: AppStyles.rkRegularTextStyle(size: AppConstants.font_12, color: AppColors.redColor,fontWeight: FontWeight.w500).copyWith(decoration: TextDecoration.lineThrough,decorationColor: AppColors.redColor),),
-                          2.height,
-                         CommonProductButtonWidget(
-                           title:
-                           "${AppLocalizations.of(context)!.currency}${discountedPrice.toStringAsFixed(AppConstants.amountFrLength) == "0.00" ? '0' : discountedPrice.toStringAsFixed(AppConstants.amountFrLength)}",
-                           onPressed: onButtonTap,
-                           textColor: AppColors.whiteColor,
-                           bgColor: AppColors.mainColor,
-                           borderRadius: AppConstants.radius_3,
-                           textSize: AppConstants.font_14,
-                           height: 32,
-                           width: 80,
-                         ),
-                       ],
-                     ) : 0.width,
-                   ],
-                 ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        salesDesc!.isNotEmpty?Container(
-                          padding: EdgeInsets.all(3),
-                          margin: EdgeInsets.zero,
-                          decoration: BoxDecoration(color: AppColors.saleBGColor, border: Border.all(color: AppColors.saleBGColor), borderRadius: BorderRadius.circular(AppConstants.radius_3)),
-                          child: Text(
-                            "${parse(salesDesc).body?.text}",
-                            style: AppStyles.rkRegularTextStyle(size: AppConstants.font_12, color: AppColors.whiteColor,fontWeight: FontWeight.w500),
-                            maxLines: 3,
-                            textAlign: TextAlign.center,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ):0.width,
-                        !isGuestUser? numberOfUnits !='0' && price != 0.0 ? isFromSale!?Text.rich(TextSpan(
-                          text: '${AppLocalizations.of(context)?.price_par_box} ',
-                          style: AppStyles.rkRegularTextStyle(
-                              size: AppConstants.font_12, color: AppColors.blackColor),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: '${AppLocalizations.of(context)?.currency}${(price * int.parse(numberOfUnits)).toStringAsFixed(2)} ',
-                              style: AppStyles.rkRegularTextStyle(
-                                  size: AppConstants.font_12, color: AppColors.blackColor).copyWith(decoration: TextDecoration.lineThrough),
-                            ),
-                            TextSpan(
-                              text: ' ${AppLocalizations.of(context)?.currency}${(price * int.parse(numberOfUnits)).toStringAsFixed(2)}',
-                              style: AppStyles.rkRegularTextStyle(
-                                  size: AppConstants.font_12, color: AppColors.redColor),
-                            ),
-                          ],
-                        ),
-                        ) :Text(
-                          '${AppLocalizations.of(context)?.price_par_box}${' '}${AppLocalizations.of(context)?.currency}${(price * int.parse(numberOfUnits)).toStringAsFixed(2)}',
-                          style: AppStyles.rkBoldTextStyle(
-                              size: AppConstants.font_12,
-                              color: AppColors.blueColor,
-                              fontWeight: FontWeight.w400),
-                        ): 0.width : 0.width,
-                      ],
-                    )
-                  ],
-                ),
-
-              ],
-            ),
-
-
-          ],
-        ),
-      ),
-    );
-  }
 
   void showProductDetails({
     required BuildContext context,
