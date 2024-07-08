a4922e32bc58ab7eb0171012f7b5cb061ad5f23f
komal-sit <komal.a@shaligraminfotech.com>
2024-07-08 15:44:30 +0530

    change color

--- lib/ui/utils/themes/app_urls.dart
+++ lib/ui/utils/themes/app_urls.dart
@@ -1,6 +1,6 @@
 class AppUrls {
- static const String baseUrl = 'https://api.foodstock.shtibel.com/api'; ///prodUrl
-// static const String baseUrl = 'https://devapi.foodstock.shtibel.com/api'; ///devUrl
+ //static const String baseUrl = 'https://api.foodstock.shtibel.com/api'; ///prodUrl
+static const String baseUrl = 'https://devapi.foodstock.shtibel.com/api'; ///devUrl
 //static const String baseUrl = 'http://192.168.1.204:3000/api'; ///localUrl
 
   static const String baseFileUrl = 'https://foodstock-buckets.s3.il-central-1.amazonaws.com/';
